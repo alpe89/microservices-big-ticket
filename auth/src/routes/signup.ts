c48b4d6bbdabdd35fbfd3552972bc9e5f78a16bb
Alberto Pertusi <alberto.pertusi@immobiliare.it>
2020-10-26 14:32:08 +0100

    Sign in route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,38 +1,47 @@
-import { Router, Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-errors";
-import { BadRequestError } from "../errors/bad-request-error";
-import { User } from "../models/user";
+import { Router, Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = Router();
 
-router.post('/api/users/signup', [
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20})
-        .withMessage('Password must be between 4 and 20 characters')
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
+router.post(
+    '/api/users/signup',
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password must be between 4 and 20 characters')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
 
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            throw new BadRequestError('Email already in use');
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+
+        // Generate JWT
+        const userJwt = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_KEY!
+        );
+        // Store on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(201).send(user);
     }
-
-    const { email, password } = req.body;
-
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-        throw new BadRequestError('Email already in use');
-    }
-
-    const user = User.build({ email, password });
-    await user.save();
-
-    res.status(201).send(user);
-});
+);
 
 export { router as signupRouter };
